0042b9ba5a1607820205d593e9388e66b2807bf8
Andreas Rammhold <andreas@rammhold.de>
2020-11-08 01:05:27 +0100

    views: add simple issue detail view

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
-from .views import LoginView, index, list_advisories, list_issues
+from .views import IssueDetail, LoginView, index, list_advisories, list_issues
 
 auth_urls = [
     path("login/", LoginView.as_view(), name="login"),
@@ -12,5 +12,6 @@ urlpatterns = [
     path("accounts/", include((auth_urls, "auth"))),
     path("advisories/", view=list_advisories, name="advisories"),
     path("issues/", view=list_issues, name="issues"),
+    path("issues/<str:identifier>", IssueDetail.as_view(), name="issue_detail"),
     path("", view=index, name="index"),
 ]
