7b772374b12686f4c6a89e77f1af84a8e16a8b5e
Martin Weinelt <hexa@darmstadt.ccc.de>
2020-11-10 01:49:18 +0100

    tests: account for pagination in test_list_issues

--- tracker/tests/test_views.py
+++ tracker/tests/test_views.py
@@ -1,6 +1,8 @@
+import itertools
 from typing import List
 
 import pytest
+from django.conf import settings
 from django.contrib.auth import get_user as auth_get_user
 from django.contrib.auth.models import User
 from django.urls import reverse
@@ -77,14 +79,21 @@ def test_list_empty_issues(client):
 
 @pytest.mark.django_db
 def test_list_issues(client):
-    issues = IssueFactory.create_batch(100)
+    num_pages = 3
+    issues = IssueFactory.create_batch(settings.PAGINATE_BY * num_pages)
 
-    response = client.get(reverse("issues"))
-    assert response.status_code == 200
-    assertTemplateUsed(response, "issues/list.html")
-    for issue in issues:
-        assert issue.identifier in response.content.decode("utf-8")
-        assert issue.description in response.content.decode("utf-8")
+    i = iter(issues)
+    slices = [itertools.islice(iter(i), settings.PAGINATE_BY) for _ in range(num_pages)]
+
+    for page, issue_chunk in enumerate(slices, start=1):
+        response = client.get(reverse("issues"), {"page": page})
+        assert response.status_code == 200
+        assertTemplateUsed(response, "issues/list.html")
+        content = response.content.decode("utf-8")
+
+        for issue in issue_chunk:
+            assert issue.identifier in content
+            assert issue.description in content
 
 
 @pytest.mark.django_db
