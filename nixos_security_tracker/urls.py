b07597690eca48dd0c9c136bcb8c75a98f002018
Andreas Rammhold <andreas@rammhold.de>
2020-11-11 13:34:31 +0100

    Introduce django_debug_toolbar
    This allows us to inspect templates, variables and SQL queries while
    running in debug mode.

--- nixos_security_tracker/urls.py
+++ nixos_security_tracker/urls.py
@@ -13,10 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [
+prepend = []
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    prepend.append(path("__debug__/", include(debug_toolbar.urls)))
+
+
+urlpatterns = prepend + [
     path("admin/", admin.site.urls),
     path("", include("tracker.urls")),
 ]
