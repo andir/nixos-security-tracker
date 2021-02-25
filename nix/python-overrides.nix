098b7b17647f5486c3967576f6a260d51e26f10d
Andreas Rammhold <andreas@rammhold.de>
2021-02-25 18:13:23 +0100

    Update python dependencies

--- nix/python-overrides.nix
+++ nix/python-overrides.nix
@@ -1,10 +1,8 @@
 { poetry2nix }:
 poetry2nix.defaultPoetryOverrides.extend (self: super: {
-  packaging = super.packaging.overridePythonAttrs (
+  cwcwidth = super.cwcwidth.overridePythonAttrs (
     old: {
-      # packaging now requires flit_core to be built:
-      # https://github.com/nix-community/poetry2nix/issues/218
-      buildInputs = [ super.flit-core ];
+      nativeBuildInputs = [ super.cython ];
     }
   );
 })
