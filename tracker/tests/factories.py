acd45ef94ead25d022eb4db41b0c68e5ad076e99
Andreas Rammhold <andreas@rammhold.de>
2021-05-12 16:25:01 +0200

    Add models to store Packages and Channels
    This is a first step towards importing the packages that are available
    in Nixpkgs into the application. Going from here we can start working on
    importing the channels (history) and how to identify individual packages
    as affected by a given CVE.

--- tracker/tests/factories.py
+++ tracker/tests/factories.py
@@ -2,7 +2,7 @@ import datetime
 
 from factory import Sequence, SubFactory
 from factory.django import DjangoModelFactory
-from factory.fuzzy import FuzzyDateTime
+from factory.fuzzy import FuzzyDateTime, FuzzyText
 from pytz import UTC
 
 from tracker import models
@@ -30,3 +30,51 @@ class AdvisoryFactory(DjangoModelFactory):
         model = models.Advisory
 
     nsa_id = Sequence(lambda n: f"NSA{n}")
+
+
+class ChannelFactory(DjangoModelFactory):
+    class Meta:
+        model = models.Channel
+
+    name = Sequence(lambda n: f"Channel-{n}")
+
+
+class ChannelRevisionFactory(DjangoModelFactory):
+    class Meta:
+        model = models.ChannelRevision
+
+    name = Sequence(lambda n: f"Revision {n}")
+    git_revision = Sequence(lambda n: f"{n}")
+    channel = SubFactory(ChannelFactory)
+    datetime_created = FuzzyDateTime(datetime.datetime(1998, 1, 1, tzinfo=UTC))
+
+
+class PackageAttributeNameFactory(DjangoModelFactory):
+    class Meta:
+        model = models.PackageAttributeName
+
+    value = FuzzyText(length=20)
+
+
+class PackageNameFactory(DjangoModelFactory):
+    class Meta:
+        model = models.PackageName
+
+    value = FuzzyText(length=20)
+
+
+class PackageVersionFactory(DjangoModelFactory):
+    class Meta:
+        model = models.PackageVersion
+
+    value = FuzzyText(length=5)
+
+
+class PackageFactory(DjangoModelFactory):
+    class Meta:
+        model = models.Package
+
+    attribute_name_row = SubFactory(PackageAttributeNameFactory)
+    name_row = SubFactory(PackageNameFactory)
+    version_row = SubFactory(PackageVersionFactory)
+    channel_revision = SubFactory(ChannelRevisionFactory)
